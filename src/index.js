bb1d5f38fd56ff38f37dd017355c37bf5a61bf81
abrahamjum <abrahamjum@gmail.com>
2017-12-31 18:55:59 -0800

    Initialize project

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './js/components/App';
+import Layout from './js/components/Layout'
+
+ReactDOM.render(<Layout />, document.getElementById('root'));
---
+++ src/js/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>This is App component</h1>
+      </div>
+    );
+  }
+}
